cd34c0f71b3e59050cc4fa465d019b71d67eb942
Guru901 <avinashkaur842@gmail.com>
2025-02-27 13:34:31 +0530

    Add PreOnboarding component for user redirection based on onboarding status

---
+++ src/app/pre-onboarding/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import useGetUser from "@/hooks/use-get-user";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function PreOnboarding() {
+  const user = useKindeAuth().user;
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
+
+  const { localUser } = useGetUser();
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        setLoading(true);
+        if (localUser) {
+          if (localUser.isOnBoarded) {
+            router.push(`/`);
+          } else {
+            router.push(`/onboarding`);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [user, localUser, router]);
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-xl">Redirecting....</h1>
+    </div>
+  );
+}
