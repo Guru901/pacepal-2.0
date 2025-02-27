c5d8d5afb1b8aa9673c0b09db11d9ad04b489d69
Guru901 <avinashkaur842@gmail.com>
2025-02-28 02:15:25 +0530

    Replace process.env with env

--- src/server/database/connectToDb.ts
+++ src/server/database/connectToDb.ts
@@ -1,9 +1,10 @@
+import { env } from "@/env";
 import * as mongoose from "mongoose";
 
 export async function connectToDB() {
   try {
     if (mongoose.connection.readyState) return;
-    await mongoose.connect(process.env.DATABASE_URL!);
+    await mongoose.connect(env.DATABASE_URL);
     console.log(`Connected to database`);
   } catch (error: unknown) {
     console.error(`Error connecting to database: ${String(error)}`);
