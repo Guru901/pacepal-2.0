ad6a75f8937f4219960f0eec3c17e3668a584d0f
Guru901 <avinashkaur842@gmail.com>
2025-02-28 18:07:26 +0530

    Refactor form validation logic to enhance user experience and improve error handling

--- src/server/api/routers/charts.ts
+++ src/server/api/routers/charts.ts
@@ -2,292 +2,325 @@ import { GetChartsDataSchema } from "@/lib/schema";
 import { createTRPCRouter, dbProcedure } from "@/server/api/trpc";
 import { Form } from "@/server/database/models/form-model";
 import { User } from "@/server/database/models/user-model";
+import { TRPCError } from "@trpc/server";
 
 export const chartsRouter = createTRPCRouter({
   getDistractionsData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version } = input;
+      try {
+        const { id, version } = input;
 
-      const distractionsArray: string[] = [];
-      const distractions: string[] = [];
+        const distractionsArray: string[] = [];
+        const distractions: string[] = [];
 
-      const forms = (await Form.find({
-        createdBy: id,
-        version,
-      }).select("distractionsList createdAt")) as [];
+        const forms = (await Form.find({
+          createdBy: id,
+          version,
+        }).select("distractionsList createdAt")) as [];
 
-      forms.forEach((form: { distractionsList: string }) => {
-        form.distractionsList.split(",").forEach((distraction) => {
-          distractionsArray.push(distraction.trim().replaceAll(" ", ""));
+        forms.forEach((form: { distractionsList: string }) => {
+          form.distractionsList.split(",").forEach((distraction) => {
+            distractionsArray.push(distraction.trim().replaceAll(" ", ""));
+          });
         });
-      });
 
-      distractionsArray.forEach((distraction) => {
-        if (distraction !== "") {
-          distractions.push(distraction);
+        distractionsArray.forEach((distraction) => {
+          if (distraction !== "") {
+            distractions.push(distraction);
+          }
+        });
+
+        if (!forms.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { distractions: [] },
+          };
         }
-      });
-
-      if (!forms.length) {
         return {
-          message: "Forms not found",
           success: true,
-          data: { distractions: [] },
+          message: "User Found",
+          data: { distractions: distractions },
         };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-      return {
-        success: true,
-        message: "User Found",
-        data: { distractions: distractions },
-      };
     }),
 
   getMoodData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version } = input;
-      const forms = (await Form.find({
-        createdBy: id,
-        version,
-      }).select("mood createdAt")) as [];
+      try {
+        const { id, version } = input;
+        const forms = (await Form.find({
+          createdBy: id,
+          version,
+        }).select("mood createdAt")) as [];
 
-      if (!forms.length) {
+        if (!forms.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { forms: [] },
+          };
+        }
         return {
-          message: "Forms not found",
+          message: "Forms found",
           success: true,
-          data: { forms: [] },
+          data: { forms },
         };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-      return {
-        message: "Forms found",
-        success: true,
-        data: { forms },
-      };
     }),
 
   getOverworkData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version } = input;
-      let totalOverWorkHours = 0;
+      try {
+        const { id, version } = input;
+        let totalOverWorkHours = 0;
 
-      const forms = (await Form.find({ version, createdBy: id })) as [];
+        const forms = (await Form.find({ version, createdBy: id })) as [];
 
-      forms.forEach((form: { overWork: number }) => {
-        totalOverWorkHours += form.overWork;
-      });
+        forms.forEach((form: { overWork: number }) => {
+          totalOverWorkHours += form.overWork;
+        });
 
-      return {
-        success: true,
-        data: {
-          overWork: totalOverWorkHours,
-        },
-      };
+        return {
+          success: true,
+          data: {
+            overWork: totalOverWorkHours,
+          },
+        };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
+      }
     }),
 
   getPenaltyData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version: versionName } = input;
-      let penalty = 0;
+      try {
+        const { id, version: versionName } = input;
+        let penalty = 0;
 
-      const user = await User.findById(id);
+        const user = await User.findById(id);
 
-      const desiredSleepHours = user?.versions
-        ?.map(
-          (version: {
-            versionName: string | null;
-            data: { desiredSleepHours: number };
-          }) => {
-            if (version.versionName === versionName) {
-              return version.data?.desiredSleepHours;
+        const desiredSleepHours = user?.versions
+          ?.map(
+            (version: {
+              versionName: string | null;
+              data: { desiredSleepHours: number };
+            }) => {
+              if (version.versionName === versionName) {
+                return version.data?.desiredSleepHours;
+              }
+              return null;
+            },
+          )
+          .filter((hours: number | null) => {
+            return hours !== null;
+          });
+
+        const forms = (await Form.find({
+          createdBy: id,
+          versionName,
+        }).select("hoursSlept")) as [];
+
+        if (desiredSleepHours) {
+          forms.forEach((form: { hoursSlept: number }) => {
+            if (form.hoursSlept > Number(desiredSleepHours[0])) {
+              const penaltyHrs = form.hoursSlept - Number(desiredSleepHours[0]);
+              penalty += penaltyHrs * 100;
             }
-            return null;
-          },
-        )
-        .filter((hours: number | null) => {
-          return hours !== null;
-        });
+          });
+        }
 
-      const forms = (await Form.find({
-        createdBy: id,
-        versionName,
-      }).select("hoursSlept")) as [];
-
-      if (desiredSleepHours) {
-        forms.forEach((form: { hoursSlept: number }) => {
-          if (form.hoursSlept > Number(desiredSleepHours[0])) {
-            const penaltyHrs = form.hoursSlept - Number(desiredSleepHours[0]);
-            penalty += penaltyHrs * 100;
-          }
-        });
+        return {
+          success: true,
+          data: { penalty },
+        };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      return {
-        success: true,
-        data: { penalty },
-      };
     }),
 
   getProductivityData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version } = input;
+      try {
+        const { id, version } = input;
 
-      const productivityData: {
-        date: string;
-        productivity: string;
-      }[] = [];
+        const productivityData: {
+          date: string;
+          productivity: string;
+        }[] = [];
 
-      const forms = (await Form.find({
-        createdBy: id,
-        version,
-      })
-        .select("productivity createdAt")
-        .sort({
-          createdAt: -1,
+        const forms = (await Form.find({
+          createdBy: id,
+          version,
         })
-        .limit(30)) as [];
+          .select("productivity createdAt")
+          .sort({
+            createdAt: -1,
+          })
+          .limit(30)) as [];
 
-      if (!forms.length) {
-        return {
-          message: "Forms not found",
-          success: true,
-          data: { productivityData: [] },
-        };
-      }
+        if (!forms.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { productivityData: [] },
+          };
+        }
 
-      forms.forEach((form: { productivity: string; createdAt: Date }) => {
-        const date = form.createdAt.toLocaleDateString().split("/");
-        const formattedDate = `${date[1]}/${date[0]}/${date[2]}`;
-        productivityData.push({
-          productivity: form.productivity,
-          date: formattedDate,
+        forms.forEach((form: { productivity: string; createdAt: Date }) => {
+          const date = form.createdAt.toLocaleDateString().split("/");
+          const formattedDate = `${date[1]}/${date[0]}/${date[2]}`;
+          productivityData.push({
+            productivity: form.productivity,
+            date: formattedDate,
+          });
         });
-      });
 
-      return {
-        success: true,
-        data: { productivityData },
-      };
+        return {
+          success: true,
+          data: { productivityData },
+        };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
+      }
     }),
 
   getSleepData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version: versionFromClient } = input;
-      const forms = (await Form.find({
-        createdBy: id,
-        version: versionFromClient,
-      }).select("hoursSlept createdAt")) as [];
+      try {
+        const { id, version: versionFromClient } = input;
+        const forms = (await Form.find({
+          createdBy: id,
+          version: versionFromClient,
+        }).select("hoursSlept createdAt")) as [];
 
-      const user = await User.findById(id);
+        const user = await User.findById(id);
 
-      const desiredSleepHours = user?.versions
-        ?.map(
-          (version: {
-            versionName: string | null;
-            data: { desiredSleepHours: number };
-          }) => {
-            if (version.versionName === versionFromClient) {
-              return version.data?.desiredSleepHours;
-            }
-            return null;
-          },
-        )
-        .filter((hours: number | null) => {
-          return hours !== null;
-        });
+        const desiredSleepHours = user?.versions
+          ?.map(
+            (version: {
+              versionName: string | null;
+              data: { desiredSleepHours: number };
+            }) => {
+              if (version.versionName === versionFromClient) {
+                return version.data?.desiredSleepHours;
+              }
+              return null;
+            },
+          )
+          .filter((hours: number | null) => {
+            return hours !== null;
+          });
 
-      let singleDesiredSleepingHours;
+        let singleDesiredSleepingHours;
 
-      if (desiredSleepHours) {
-        singleDesiredSleepingHours =
-          desiredSleepHours.length > 0 ? [desiredSleepHours[0]] : [];
-      }
+        if (desiredSleepHours) {
+          singleDesiredSleepingHours =
+            desiredSleepHours.length > 0 ? [desiredSleepHours[0]] : [];
+        }
 
-      if (!forms.length) {
+        if (!forms.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { forms: [], desiredSleepHours: [] },
+          };
+        }
+
         return {
-          message: "Forms not found",
           success: true,
-          data: { forms: [], desiredSleepHours: [] },
+          data: { forms: forms, desiredSleepHours: singleDesiredSleepingHours },
         };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      return {
-        success: true,
-        data: { forms: forms, desiredSleepHours: singleDesiredSleepingHours },
-      };
     }),
 
   getTodosData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version } = input;
-      const todos = (await Form.find({
-        createdBy: id,
-        version,
-      }).select("tasksCompleted tasksPlanned createdAt")) as [];
+      try {
+        const { id, version } = input;
+        const todos = (await Form.find({
+          createdBy: id,
+          version,
+        }).select("tasksCompleted tasksPlanned createdAt")) as [];
 
-      if (!todos.length) {
+        if (!todos.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { todos: [] },
+          };
+        }
+
         return {
-          message: "Forms not found",
           success: true,
-          data: { todos: [] },
+          data: { todos },
         };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      return {
-        success: true,
-        data: { todos },
-      };
     }),
 
   getWorkData: dbProcedure
     .input(GetChartsDataSchema)
     .query(async ({ input }) => {
-      const { id, version: versionFromClient } = input;
-      const forms = (await Form.find({
-        createdBy: id,
-        version: versionFromClient,
-      }).select("hoursPlanned hoursWorked createdAt")) as [];
+      try {
+        const { id, version: versionFromClient } = input;
+        const forms = (await Form.find({
+          createdBy: id,
+          version: versionFromClient,
+        }).select("hoursPlanned hoursWorked createdAt")) as [];
 
-      const user = await User.findById(id);
+        const user = await User.findById(id);
 
-      const desiredWorkingHours = user?.versions
-        ?.map(
-          (version: {
-            versionName: string;
-            data: { slots: { name: string; hours: number }[] };
-          }) => {
-            if (version.versionName === versionFromClient) {
-              return version.data.slots;
-            }
-            return null;
-          },
-        )
-        .filter((slots) => {
-          return slots !== null;
-        });
+        const desiredWorkingHours = user?.versions
+          ?.map(
+            (version: {
+              versionName: string;
+              data: { slots: { name: string; hours: number }[] };
+            }) => {
+              if (version.versionName === versionFromClient) {
+                return version.data.slots;
+              }
+              return null;
+            },
+          )
+          .filter((slots) => {
+            return slots !== null;
+          });
 
-      let singleDesiredWorkingHours;
-      if (desiredWorkingHours) {
-        singleDesiredWorkingHours =
-          desiredWorkingHours.length > 0 ? [desiredWorkingHours[0]] : [];
-      }
+        let singleDesiredWorkingHours;
+        if (desiredWorkingHours) {
+          singleDesiredWorkingHours =
+            desiredWorkingHours.length > 0 ? [desiredWorkingHours[0]] : [];
+        }
 
-      if (!forms.length) {
+        if (!forms.length) {
+          return {
+            message: "Forms not found",
+            success: true,
+            data: { forms: [], desiredWorkingHours },
+          };
+        }
+
         return {
-          message: "Forms not found",
           success: true,
-          data: { forms: [], desiredWorkingHours },
+          data: { forms, desiredWorkingHours: singleDesiredWorkingHours },
         };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      return {
-        success: true,
-        data: { forms, desiredWorkingHours: singleDesiredWorkingHours },
-      };
     }),
 });
