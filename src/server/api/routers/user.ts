2a69083a396932da20602c3a351f1a2828af084b
Guru901 <avinashkaur842@gmail.com>
2025-02-28 18:08:43 +0530

    Implement error handling in user onboarding and update methods; add try-catch blocks for improved reliability

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -5,118 +5,131 @@ import {
   UpdateSleepSchema,
   UpdateSlotsSchema,
 } from "@/lib/schema";
+import { TRPCError } from "@trpc/server";
 
 export const userRouter = createTRPCRouter({
   onboardUser: dbProcedure
     .input(OnBoardUserSchema)
     .mutation(async ({ input }) => {
-      const {
-        email,
-        id,
-        picture,
-        given_name,
-        isOnBoarded,
-        slots,
-        desiredSleepHours,
-        version,
-      } = input;
+      try {
+        const {
+          email,
+          id,
+          picture,
+          given_name,
+          isOnBoarded,
+          slots,
+          desiredSleepHours,
+          version,
+        } = input;
 
-      const user = await User.findOne({ kindeId: id });
+        const user = await User.findOne({ kindeId: id });
 
-      if (user) {
-        return { message: "User already exists", success: false };
-      }
+        if (user) {
+          return { message: "User already exists", success: false };
+        }
 
-      await User.create({
-        email,
-        kindeId: id,
-        picture,
-        given_name,
-        isOnBoarded,
-        versions: [
-          {
-            versionName: version,
-            data: {
-              slots,
-              desiredSleepHours,
+        await User.create({
+          email,
+          kindeId: id,
+          picture,
+          given_name,
+          isOnBoarded,
+          versions: [
+            {
+              versionName: version,
+              data: {
+                slots,
+                desiredSleepHours,
+              },
             },
-          },
-        ],
-      });
+          ],
+        });
 
-      return {
-        message: "User Onboarded",
-        success: true,
-      };
+        return {
+          message: "User Onboarded",
+          success: true,
+        };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
+      }
     }),
 
   updateSlots: dbProcedure
     .input(UpdateSlotsSchema)
     .mutation(async ({ input }) => {
-      const { id, slots, version } = input;
+      try {
+        const { id, slots, version } = input;
 
-      const user = await User.findByIdAndUpdate(
-        id,
-        {
-          $set: {
-            "versions.$[elem].data.slots": slots,
+        const user = await User.findByIdAndUpdate(
+          id,
+          {
+            $set: {
+              "versions.$[elem].data.slots": slots,
+            },
           },
-        },
-        {
-          arrayFilters: [{ "elem.versionName": version }],
-          new: true,
-        },
-      );
+          {
+            arrayFilters: [{ "elem.versionName": version }],
+            new: true,
+          },
+        );
 
-      if (!user) {
-        return {
-          message: "User not saved yet",
-          success: false,
-          data: { versions: [] },
-        };
+        if (!user) {
+          return {
+            message: "User not saved yet",
+            success: false,
+            data: { versions: [] },
+          };
+        }
+
+        const versions = user.versions;
+
+        return { success: true, user, data: { versions } };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      const versions = user.versions;
-
-      return { success: true, user, data: { versions } };
     }),
 
   updateSleep: dbProcedure
     .input(UpdateSleepSchema)
     .mutation(async ({ input }) => {
-      const { id, desiredSleepHours, version } = input;
+      try {
+        const { id, desiredSleepHours, version } = input;
 
-      if (!id || !desiredSleepHours) {
-        return {
-          message: "Missing required parameters",
-          success: false,
-          data: { versions: [] },
-        };
-      }
+        if (!id || !desiredSleepHours) {
+          return {
+            message: "Missing required parameters",
+            success: false,
+            data: { versions: [] },
+          };
+        }
 
-      const user = await User.findByIdAndUpdate(
-        id,
-        {
-          $set: {
-            "versions.$[elem].data.desiredSleepHours": desiredSleepHours,
+        const user = await User.findByIdAndUpdate(
+          id,
+          {
+            $set: {
+              "versions.$[elem].data.desiredSleepHours": desiredSleepHours,
+            },
           },
-        },
-        {
-          arrayFilters: [{ "elem.versionName": version }],
-          new: true,
-        },
-      );
+          {
+            arrayFilters: [{ "elem.versionName": version }],
+            new: true,
+          },
+        );
 
-      if (!user) {
-        return {
-          message: "User not saved yet",
-          success: false,
-          data: { versions: [] },
-        };
+        if (!user) {
+          return {
+            message: "User not saved yet",
+            success: false,
+            data: { versions: [] },
+          };
+        }
+
+        const versions = user.versions;
+
+        return { success: true, user, data: { versions } };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
       }
-
-      const versions = user.versions;
-
-      return { success: true, user, data: { versions } };
     }),
 });
