3bb72c7b7fb07083aee5de213c2da323022f7d6c
Guru901 <avinashkaur842@gmail.com>
2025-02-28 18:08:02 +0530

    Enhance error handling in form submission and query methods; implement try-catch blocks for improved reliability

--- src/server/api/routers/form.ts
+++ src/server/api/routers/form.ts
@@ -2,76 +2,85 @@ import { z } from "zod";
 import { createTRPCRouter, dbProcedure } from "../trpc";
 import { SubmitFormSchema } from "@/lib/schema";
 import { Form } from "@/server/database/models/form-model";
+import { TRPCError } from "@trpc/server";
 
 export const formRouter = createTRPCRouter({
   isFormSubmitted: dbProcedure.input(z.string()).query(async ({ input }) => {
-    const startOfToday = new Date();
-    startOfToday.setUTCHours(0, 0, 0, 0);
+    try {
+      const startOfToday = new Date();
+      startOfToday.setUTCHours(0, 0, 0, 0);
 
-    const endOfToday = new Date();
-    endOfToday.setUTCHours(23, 59, 59, 999);
+      const endOfToday = new Date();
+      endOfToday.setUTCHours(23, 59, 59, 999);
 
-    const form = (await Form.find({
-      createdBy: input,
-      createdAt: {
-        $gte: startOfToday,
-        $lte: endOfToday,
-      },
-    })) as [];
+      const form = (await Form.find({
+        createdBy: input,
+        createdAt: {
+          $gte: startOfToday,
+          $lte: endOfToday,
+        },
+      })) as [];
 
-    if (!form.length) {
+      if (!form.length) {
+        return {
+          message: "Form not found",
+          success: true,
+          data: { isFormSubmitted: false },
+        };
+      }
       return {
-        message: "Form not found",
+        message: "Form submitted",
         success: true,
-        data: { isFormSubmitted: false },
+        data: { isFormSubmitted: true },
       };
+    } catch (error) {
+      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
     }
-    return {
-      message: "Form submitted",
-      success: true,
-      data: { isFormSubmitted: true },
-    };
   }),
 
   submitForm: dbProcedure
     .input(SubmitFormSchema)
     .mutation(async ({ input }) => {
-      const {
-        userId,
-        productivity,
-        hoursWorked,
-        hoursPlanned,
-        tasksCompleted,
-        tasksPlanned,
-        sleptWell,
-        distractions,
-        distractionsList,
-        mood,
-        followedSchedule,
-        hoursSlept,
-        version,
-        overWork,
-        createdBy,
-      } = input;
+      try {
+        const {
+          userId,
+          productivity,
+          hoursWorked,
+          hoursPlanned,
+          tasksCompleted,
+          tasksPlanned,
+          sleptWell,
+          distractions,
+          distractionsList,
+          mood,
+          followedSchedule,
+          hoursSlept,
+          version,
+          overWork,
+          createdBy,
+        } = input;
 
-      const form = await Form.create({
-        userId,
-        productivity,
-        hoursWorked,
-        hoursPlanned,
-        tasksCompleted,
-        tasksPlanned,
-        sleptWell,
-        distractions,
-        distractionsList,
-        mood,
-        followedSchedule,
-        hoursSlept,
-        version,
-        overWork,
-        createdBy,
-      });
+        const form = await Form.create({
+          userId,
+          productivity,
+          hoursWorked,
+          hoursPlanned,
+          tasksCompleted,
+          tasksPlanned,
+          sleptWell,
+          distractions,
+          distractionsList,
+          mood,
+          followedSchedule,
+          hoursSlept,
+          version,
+          overWork,
+          createdBy,
+        });
 
-      return { message: "Form submitted", success: true, data: form };
+        return { message: "Form submitted", success: true, data: form };
+      } catch (error) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: error });
+      }
     }),
 });
